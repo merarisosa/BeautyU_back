2725abeb21be53081a05fb79faf08311a78ec44a
Merari May Sosa <merarimaysosa@gmail.com>
2024-08-13 00:15:54 -0600

    Creacion del backend del proyecto BeautyU

---
+++ src/main/java/com/beautyU/BeautyU/BeautyUApplication.java
@@ -0,0 +1,13 @@
+package com.beautyU.BeautyU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beautyU/BeautyU/BeautyUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautyU.BeautyU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
